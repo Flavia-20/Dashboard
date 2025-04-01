356103db8ec8f864343a1ae2412cec17794cb45f
Flávia Monique <flaviamonique38@gmail.com>
2025-03-31 23:32:58 -0300

    conexão com o banco

--- vendas.py
+++ vendas.py
@@ -1,10 +1,31 @@
 import pyodbc
 
-dados_empresa = (
-    "Driver={SQL Server};"
-    "Server=LAPTOP-C6FDP5R6;"
-    "Database=dbproinfo;"
-)
+import streamlit as st
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+import plotly.express as px
 
-conexao = pyodbc.connect(dados_empresa)
-print("conectado")
+def conectar_ao_banco():
+    dados_empresa = (
+        'DRIVER={SQL Server Native Client 11.0};'
+        'SERVER=aquidaba.infonet.com.br;'
+        'DATABASE=dbproinfo;'
+        'UID=leituraVendas;'
+        'PWD=***********;'
+    )
+
+    conexao = pyodbc.connect(dados_empresa)
+    cursor = conexao.cursor()
+
+    return conexao, cursor
+
+conexao, cursor = conectar_ao_banco()
+
+consulta = cursor.execute("SELECT * FROM tbVendasDashboard where nmFilial ='FILIAL 0001';").fetchall()
+#print(consulta)
+
+
+
+
+
